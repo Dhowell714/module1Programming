292d343365c3b86fd33233aa5223ed9727a6bfa4
Luna Silver <drhowell714@gmail.com>
2022-01-19 21:07:50 -0500

    1

---
+++ HowellDevinM01Ex1.py
@@ -0,0 +1,3 @@
+C = int(input("Enter temperature in Celsius: "))
+F = 9/5*C+32
+print(str(F))
---
+++ HowellDevinM01Ex2.py
@@ -0,0 +1,5 @@
+meal = int(input("Enter price of meal: "))
+tip = 18 % meal
+salesTax = 7
+total = meal + salesTax + tip
+print(total)
